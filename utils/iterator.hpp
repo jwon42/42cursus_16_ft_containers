4d955202bce9d981ebcc31156b4a29f7923aa0de
jwon42 <jwon@student.42seoul.kr>
2021-08-15 20:40:24 +0900

    feat: add iterator, reverse_iterator

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,39 +6,153 @@
 /*   By: jwon <jwon@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 14:15:05 by jwon              #+#    #+#             */
-/*   Updated: 2021/08/15 16:05:33 by jwon             ###   ########.fr       */
+/*   Updated: 2021/08/15 20:40:03 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
-# include <cstddef>
 
 namespace ft {
+  /* ITERATOR TAGS */
   struct input_iterator_tag {};
   struct output_iterator_tag : public input_iterator_tag {};
   struct forward_iterator_tag : public output_iterator_tag {};
   struct bidirectional_iterator_tag : public forward_iterator_tag {};
   struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
-  template <class Category, class T, bool is_const, class Distance = std::ptrdiff_t>
-  class iterator {
-    public:
-      typedef T value_type;
-      typedef Distance difference_type;
-      typedef typename std::conditional<is_const, const T*, T*>::type pointer;
-      typedef typename std::conditional<is_const, const T&, T&>::type reference;
-      typedef Category iterator_category;
+  /* STANDARD ITERATOR TRAITS */
+  template <typename Iter>
+  struct iterator_traits
+  {
+    typedef typename Iter::iterator_category iterator_category;
+    typedef typename Iter::value_type value_type;
+    typedef typename Iter::difference_type difference_type;
+    typedef difference_type distance_type;
+    typedef typename Iter::pointer pointer;
+    typedef typename Iter::reference reference;
   };
 
-  template <typename it>
-  struct iterator_traits {
+  /* ITERATOR TRAITS FOR POINTER */
+  template <typename T>
+  struct iterator_traits<T *>
+  {
+    typedef random_access_iterator_tag iterator_category;
+    typedef T value_type;
+    typedef ptrdiff_t difference_type;
+    typedef ptrdiff_t distance_type;
+    typedef T *pointer;
+    typedef T &reference;
+  };
+
+  /* ITERATOR TRAITS FOR CONST POINTER */
+  template <typename T>
+  struct iterator_traits<const T *>
+  {
+    typedef random_access_iterator_tag iterator_category;
+    typedef T value_type;
+    typedef ptrdiff_t difference_type;
+    typedef ptrdiff_t distance_type;
+    typedef T *pointer;
+    typedef T &reference;
+  };
+
+  /* ITERATOR */
+  template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+  struct iterator
+  {
+    typedef Category iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef Pointer pointer;
+    typedef Reference reference;
+  };
+
+  /* REVERSE ITERATOR */
+  template <typedef Iter>
+  class reverse_iterator
+  {
+    private:
+      Iter m_base;
+      reverse_iterator();
+
     public:
-      typedef typename it::value_type value_type;
-      typedef typename it::difference_type difference_type;
-      typedef typename it::pointer pointer;
-      typedef typename it::reference reference;
-      typedef typename it::iterator_category iterator_category;
+      typedef Iter iterator_type;
+      typedef typename iterator_traits<Iter>::iterator_category iterator_category
+      typedef typename iterator_traits<Iter>::value_type value_type;
+      typedef typename iterator_traits<Iter>::difference_type difference_type;
+      typedef typename iterator_traits<Iter>::pointer pointer;
+      typedef typename iterator_traits<Iter>::reference reference;
+
+      reverse_iterator(Iter base): m_base(base)
+      {}
+
+      reverse_iterator(const reverse_iterator<Iter> &other): m_base(other.base)
+      {}
+
+      ~reverse_iterator()
+      {}
+
+      reverse_iterator<Iter> &operator=(const reverse_iterator<Iter> &other)
+      {
+        m_base = other.base;
+        return (*this);
+      }
+
+      Iter base() const
+      {
+        return (m_base);
+      }
+
+      reference operator*() const
+      {
+        Iter iter(m_base);
+        --iter;
+        return (*iter);
+      }
+
+      pointer operator->() const
+      {
+        Iter iter(m_base);
+        --iter;
+        return (iter.operator->());
+      }
+
+      reverse_iterator<Iter> &operator++()
+      {
+        --m_base;
+        return (*this);
+      }
+
+      reverse_iterator<Iter> &operator--()
+      {
+        ++m_base;
+        return (*this);
+      }
+
+      reverse_iterator<Iter> operator++(int)
+      {
+        reverse_iterator<Iter> temp(m_base++);
+        return (temp);
+      }
+
+      reverse_iterator<Iter> operator--(int)
+      {
+        reverse_iterator<Iter> temp(m_base--);
+        return (temp);
+      }
+
+      reverse_iterator<Iter> &operator+=(difference_type offset)
+      {
+        m_base -= offset;
+        return (*this);
+      }
+
+      reverse_iterator<Iter> &operator-=(difference_type offset)
+      {
+        m_base += offset;
+        return (*this);
+      }
   };
 }
 
